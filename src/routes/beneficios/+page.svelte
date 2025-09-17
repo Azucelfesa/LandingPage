abcaf1f5e2d7b52c8cbda5da06db1f4511dc5765
Jose V <pepevaldalva@gmail.com>
2025-09-17 17:45:50 -0600

    Arreglando pagina de beneficios

--- src/routes/beneficios/+page.svelte
+++ src/routes/beneficios/+page.svelte
@@ -2,13 +2,12 @@
 
 </script>
 <svelte:head>
-    <h1>Beneficios de unirse al grupo</h1>
-    <ul>
+    <title>Beneficios de unirse al grupo</title>
+</svelte:head>
+<ul>
         <li>Información oficial y actualizada del ECOEMS.</li>
         <li>Tips prácticos para padres y alumnos.</li>
         <li>Recordatorios clave para que tu hijo no pierda fechas.</li>
         <li>Comunicación directa con asesores.</li>
         <li>Material exclusivo para preparar mejor a tu hijo.</li>
-    </ul>
-
-</svelte:head>
+</ul>
