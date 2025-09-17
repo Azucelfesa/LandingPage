dddbecd4dc557252d84f3cf94ef7cb8a97139dfe
Jose V <pepevaldalva@gmail.com>
2025-09-17 17:43:19 -0600

    Creando pagina de beneficios

---
+++ src/routes/beneficios/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+<svelte:head>
+    <h1>Beneficios de unirse al grupo</h1>
+    <ul>
+        <li>Información oficial y actualizada del ECOEMS.</li>
+        <li>Tips prácticos para padres y alumnos.</li>
+        <li>Recordatorios clave para que tu hijo no pierda fechas.</li>
+        <li>Comunicación directa con asesores.</li>
+        <li>Material exclusivo para preparar mejor a tu hijo.</li>
+    </ul>
+
+</svelte:head>
