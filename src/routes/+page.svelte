389d0655986a87736d88d6ad5e95bd9d39c2e775
Azucel <azucel.felipe@gmail.com>
2025-09-26 18:59:46 -0600

    agrege numeros de telefono

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -679,7 +679,7 @@
 
 	function joinGroup() {
 		// Redirigir a WhatsApp
-		window.open('https://wa.me/1234567890', '_blank');
+		window.open('https://wa.me/5654961193', '_blank');
 	}
 
 	function goToHomePage() {
