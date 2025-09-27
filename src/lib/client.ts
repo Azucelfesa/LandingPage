4d0ed31237af6a1bda5320829fdaca6eaef1c69e
Jose V <pepevaldalva@gmail.com>
2025-09-27 10:10:12 -0600

    Fix http server url

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,7 +4,7 @@ import type { HTTPRouter } from '@backend'; // Asegúrate de que la ruta es corr
 const trpcHttpClient = createTRPCProxyClient<HTTPRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000',
+      url: 'https://adnetdigital.org/trpc',
     }),
   ],
 });
